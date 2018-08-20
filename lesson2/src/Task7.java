1cebf72b6f34730d7225b7e31399084973f92837
SvetlanaBSA <42421614+SvetlanaBSA@users.noreply.github.com>
2018-08-20 22:03:30 +0300

    Task7. Amount output.

---
+++ lesson2/src/Task7.java
@@ -0,0 +1,46 @@
+public class Task7 {
+    public static void main(String[] args) {
+        /*Имеется целое число (любое), это число — сумма денег в рублях.
+        Вывести это число, добавив к нему слово «рублей» в правильном падеже.*/
+        int number = 48;
+        if (number >= 0) {
+            if ((number % 100) > 10 && (number % 100) < 20)
+                System.out.println(number + " рублей");
+            else
+                switch (number % 10) {
+                    case 0:
+                        System.out.println(number + " рублей");
+                        break;
+                    case 1:
+                        System.out.println(number + " рубль");
+                        break;
+                    case 2:
+                        System.out.println(number + " рубля");
+                        break;
+                    case 3:
+                        System.out.println(number + " рубля");
+                        break;
+                    case 4:
+                        System.out.println(number + " рубля");
+                        break;
+                    case 5:
+                        System.out.println(number + " рублей");
+                        break;
+                    case 6:
+                        System.out.println(number + " рублей");
+                        break;
+                    case 7:
+                        System.out.println(number + " рублей");
+                        break;
+                    case 8:
+                        System.out.println(number + " рублей");
+                        break;
+                    case 9:
+                        System.out.println(number + " рублей");
+                        break;
+            }
+        }
+        else
+            System.out.println("Error. Incorrect number.");
+    }
+}
