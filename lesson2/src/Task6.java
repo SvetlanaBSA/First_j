3333631565820f6aa68c39cf09742acd735fee04
SvetlanaBSA <42421614+SvetlanaBSA@users.noreply.github.com>
2018-08-21 00:13:51 +0300

    Problem situation removed.

--- lesson2/src/Task6.java
+++ lesson2/src/Task6.java
@@ -1,8 +1,5 @@
 public class Task6 {
     public static void main(String[] args) {
-        /*Имеется строка, которая содержит символы ? и символы #.
-        Замените все символы ? на HELLO, а # - удалите.
-        Результат вывести на экран.*/
         String text = "?#test? test# nb#?";
         if (text.length() == 0)
             System.out.println("No one character were entered.");
