577537686d022840838fbfc22b6a8edb40974351
SvetlanaBSA <42421614+SvetlanaBSA@users.noreply.github.com>
2018-08-25 00:38:35 +0300

    sort array

--- lesson4/src/SortArray.java
+++ lesson4/src/SortArray.java
@@ -1,10 +1,6 @@
 public class SortArray{
     public static void main(String[] args) {
-<<<<<<< HEAD
         int i =5;
         System.out.println(i);
-=======
-
->>>>>>> 6dea88e373bd840fd87d0bde1372ba9fbde2409b
     }
 }
